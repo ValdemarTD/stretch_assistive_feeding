ec26ec50b82dd3bdb9f320f063e24c5a68c602f5
Zachary Lee <zacharyianlee@gmail.com>
2023-06-04 13:33:30 -0700

    Bug fixes in detectface_crop.py

--- scripts/detectface_crop.py
+++ scripts/detectface_crop.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import cv2
 import rospy
 from sensor_msgs.msg import Image
@@ -5,20 +7,20 @@ from geometry_msgs.msg import Point
 from cv_bridge import CvBridge
 
 class MouthDetection_withcrop():
-    def init(self):
+    def __init__(self):
         self.face_cascade = cv2.CascadeClassifier('/home/hello-robot/catkin_ws/src/stretch_assistive_feeding/scripts/haarcascade_frontalface_default.xml')
         self.smile_cascade = cv2.CascadeClassifier('/home/hello-robot/catkin_ws/src/stretch_assistive_feeding/scripts/haarcascade_smile.xml')
         self.vid_subscriber = rospy.Subscriber('camera/color/image_raw', Image, self.camera_cb)
         self.vid_publisher = rospy.Publisher('camera/color/image_detected', Image, queue_size=3)
         self.vid_bridge = CvBridge()
-        self.mouth_publisher = rospy.Publisher('mouth_point', Point)
+        self.mouth_publisher = rospy.Publisher('mouth_point', Point, queue_size=3)
 
     def camera_cb(self, im):
-        frame = cv2.rotate(self.vid_bridge.imgmsg_to_cv2(im), cv2.ROTATE_90_CLOCKWISE)
+        frame = cv2.rotate(cv2.cvtColor(self.vid_bridge.imgmsg_to_cv2(im), cv2.COLOR_BGR2RGB), cv2.ROTATE_90_CLOCKWISE)
         #cv_image = cv2.cvtColor(self.vid_bridge.imgmsg_to_cv2(data), cv2.COLOR_BGR2RGB)
 
         frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
         faces = self.face_cascade.detectMultiScale(
                 gray,
@@ -26,6 +28,8 @@ class MouthDetection_withcrop():
                 minNeighbors=5,
                 minSize=(30, 30)
             )    
+        point_x = None
+        point_y = None
         for (x, y, w, h) in faces:
             face_frame = frame[y:y+h, x:x+w]
             face_frame = cv2.resize(face_frame, dsize=(100,100), interpolation=cv2.INTER_AREA)
@@ -34,11 +38,15 @@ class MouthDetection_withcrop():
             
             for (x1,y1,w1,h1) in mouth_rects:
                 if not h1 > 36:
-                    cv2.circle(face_frame, (int(x1+0.1*w1),y1), 3, (255,0,0), 3)
-                    cv2.circle(face_frame, (int(x1+0.9*w1),y1), 3, (0,255,0), 3)
+                    cv2.circle(frame, (int(x+x1+0.1*w1),int(y+y1+0.5*h1)), 3, (255,0,0), 3)
+                    cv2.circle(frame, (int(x+x1+0.9*w1),int(y+y1+0.5*h1)), 3, (0,255,0), 3)
+                    point_x = x+x1+0.5*w1
+                    point_y = y+y1+0.5*h1
                 break
         im_msg = self.vid_bridge.cv2_to_imgmsg(frame)
         self.vid_publisher.publish(im_msg)
+        if point_x == None or point_y == None:
+            return
         mouth_pt = Point()
         mouth_pt.x = int(x1+0.5*w1)
         mouth_pt.y = y1
